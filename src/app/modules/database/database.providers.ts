26f4817e9ba6b2c9ca0571b8039b4f77ba1cea1e
Stone C. Lasley <stonelasley@gmail.com>
2017-10-24 22:49:29 -0400

    refactor(database.providers): Pull constants up out of factory function

--- src/app/modules/database/database.providers.ts
+++ src/app/modules/database/database.providers.ts
@@ -1,14 +1,14 @@
 import * as mongoose from 'mongoose';
 
+const password = encodeURIComponent(process.env.DB_PASSWORD);
+const user = process.env.DB_USER;
+const authSrc = process.env.DB_AUTH_SOURCE;
+const host = process.env.DB_HOST;
+const connectionString = 'mongodb://' + user + ':' + password + host + authSrc;
 export const databaseProviders = [
     {
         provide: 'DbConnectionToken',
         useFactory: async () => {
-            const password = encodeURIComponent(process.env.DB_PASSWORD);
-            const user = process.env.DB_USER;
-            const authSrc = process.env.DB_AUTH_SOURCE;
-            const host = process.env.DB_HOST;
-            const connectionString = 'mongodb://' + user + ':' + password + host + authSrc;
             (mongoose as any).Promise = global.Promise;
             return await mongoose.connect(connectionString, {
                 useMongoClient: true,
