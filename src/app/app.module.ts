cd12d51fbba2c811fe3f85a97ac8e8807bdd010a
Stone C. Lasley <stonelasley@gmail.com>
2017-10-29 22:33:51 -0600

    revert(CatsModule): Remove Cats Example Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { CatsModule } from './modules/cats';
 import { AuthModule } from './modules/auth';
 import { AccountModule } from './modules/account';
 import { CommonModule } from './modules/common';
-import {ExpenseModule} from './modules/expense';
+import { ExpenseModule } from './modules/expense';
 
 @Module({
-    modules: [CatsModule, AuthModule, AccountModule, CommonModule, ExpenseModule],
+    modules: [AuthModule, AccountModule, CommonModule, ExpenseModule],
 })
 export class ApplicationModule {}
